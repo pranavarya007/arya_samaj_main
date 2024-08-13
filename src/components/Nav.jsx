999d2a23987f37c0a65b0ff4cfb2fd885737d99c
pranavarya007 <pranavarya18@gmail.com>
2024-08-14 00:40:39 +0530

    added hamburger menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,17 @@
 import { hamburger } from "../assets/icons";
 import { navLinks } from "../constants";
 import { logo } from "../assets/images";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+
   return (
     <header className='padding-x py-8 absolute z-10 w-full'>
       <nav className=' flex justify-between items-center max-container'>
@@ -43,9 +52,53 @@ const Nav = () => {
           <a href='/blog'>Blogs</a>
           </div>
         
-        <div className='hidden max-lg:block'>
+         {/* Hamburger Icon 
+          
+          
+          <div className='hidden max-lg:block'>
           <img src={hamburger} alt='hamburger icon' width={25} height={25} />
         </div>
+          
+          
+          */}
+        
+
+
+      <div className="relative hidden max-lg:block z-50">
+      {/* Hamburger Icon */}
+      
+      <button
+        onClick={toggleMenu}
+        className="text-white focus:outline-none lg:hidden"
+      >
+      <img src={hamburger} alt='hamburger icon' width={25} height={25} />
+      </button>
+
+      {/* Menu */}
+      <div
+        className={`lg:hidden fixed inset-0 bg-gray-800 bg-opacity-75 transition-transform duration-300 ease-in-out transform ${
+          isOpen ? 'translate-x-0' : '-translate-x-full'
+        }`}
+      >
+        <div className="flex flex-col items-center justify-center h-full">
+          <Link to="/" className="text-white text-xl py-4 hover:text-2xl" onClick={toggleMenu}>
+            Home
+          </Link>
+          <Link to="#about-us" className="text-white text-xl py-4 hover:text-2xl" onClick={toggleMenu}>
+            About Us
+          </Link>
+          <Link to="#services" className="text-white text-xl py-4 hover:text-2xl" onClick={toggleMenu}>
+            Our Services
+          </Link>
+          <Link to="/contact" className="text-white text-xl py-4 hover:text-2xl" onClick={toggleMenu}>
+            Contact Us
+          </Link>
+          <Link to="/blog" className="text-white text-xl py-4 hover:text-2xl" onClick={toggleMenu}>
+            Blog
+          </Link>
+        </div>
+      </div>
+    </div>
       </nav>
     </header>
   );
